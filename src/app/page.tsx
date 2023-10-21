ea40de143befd3cbeb8e3031560e622565fa4622
SimonV <sim.voyer@gmail.com>
2023-10-21 18:43:15 -0400

    Refactored map page, fixed small bug and replaced marker with sample route on the map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,28 @@
 "use client"
 
-import 'ol/ol.css';
 import React, {useEffect} from 'react';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import { fromLonLat } from 'ol/proj';
-import {Feature} from "ol"
-import {Point} from "ol/geom"
-import {Vector as VectorLayer} from "ol/layer" 
-import {Vector as SourceVector} from "ol/source"
-import {Circle, Fill, Style} from "ol/style"
+import { MapProxy } from '@/types/map/MapProxy';
+import { ServerlessApiService } from '@/services/api/ServerlessApiService';
+import { RouteShape } from '@/types/map/RouteShape';
 
 export default function Home() {
 
-  const montrealCoordinates = fromLonLat([-73.56198339521531, 45.49501768328183]);
+  useEffect(() =>  {
 
-  useEffect(() => {
-    const map = new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          source: new OSM()
-        })
-      ],
-      view: new View({
-        center: montrealCoordinates,
-        zoom: 12
-      })
-    });
+    async function fetchData(map:MapProxy){
+      const routeShape : RouteShape | null = await ServerlessApiService.getShape("shapeId");
+      if (routeShape){
+        map.addRoute(routeShape);
+      }
+    }
 
-    const vectorLayer = new VectorLayer({
-      source: new SourceVector()
-    });
+    const map = new MapProxy("map");
+    fetchData(map);
 
-    const marker = new Feature({
-      geometry: new Point(montrealCoordinates)
-    });
-
-    marker.setStyle(new Style({
-      image: new Circle({
-        radius: 5,
-        fill: new Fill({
-          color: 'black'
-        })
-      })
-    }));
-
-    vectorLayer?.getSource()?.addFeature(marker);
-    map.addLayer(vectorLayer);
+    return () => {
+      map.dispose();
+    }
   }, []);
-
 
   return (
     <main className="h-full w-full">
