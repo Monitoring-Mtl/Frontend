57e6682cbfcd1e141d5efe0cc41498c1e6cca8fa
Jeremy-Gauthier1 <83983417+Jeremy-Gauthier1@users.noreply.github.com>
2023-10-28 15:55:54 -0400

    layout de base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,39 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Monitoring MTL'
-}
+  title: "Monitoring MTL",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="map-and-side-menu grid grid-cols-12 h-full w-full">
+          <div className="header-and-map col-span-9 h-full w-full">
+            <div className="header h-12 bg-red-600">
+              <img
+                src={"/logo-pfe.png"}
+                alt={"logo"}
+                width={`${(48 * 252) / 155}`}
+                height={48}
+              />
+            </div>
+            <div className="map h-full w-full">{children}</div>
+          </div>
+          <div className="side-menu col-span-3">
+            <div className="side-menu-top h-80 border-b-2"></div>
+            <div className="side-menu-bottom"></div>
+          </div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
