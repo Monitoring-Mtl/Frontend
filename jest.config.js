36ed37a3ab169d0e7e0a25cae2b640f953222713
Hanh-Long Do <hanh-long.do.1@ens.etsmtl.ca>
2023-10-29 12:51:22 -0400

    npm test with coverage works. to do precommit

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,22 @@
 module.exports = {
     roots: ["<rootDir>/src/components/tests/"],
 
-    // Add any custom setup files if needed.
-    //setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+    // Jest configuration options
+    coverageDirectory: 'coverage', // Output directory for coverage reports
+    collectCoverage: true,        // Enable coverage collection
+    collectCoverageFrom: [
+        'src/**/*.js',              // Include your source files
+        'src/**/*.ts',
+        'src/**/*.tsx',
+    ],
+
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
 
 };
