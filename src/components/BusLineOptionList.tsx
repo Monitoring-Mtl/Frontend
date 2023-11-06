da2b7c903285a0eb7d0a96b4115577aef4c5b6a4
Hanh-Long Do <hanh-long.do.1@ens.etsmtl.ca>
2023-11-05 20:04:29 -0500

    added key and corrected var name

--- src/components/BusLineOptionList.tsx
+++ src/components/BusLineOptionList.tsx
@@ -1,6 +1,6 @@
 function BusLineOptionList(){
     const busLines = [51, 80, 480, 168]
-    const busLineOptionList = busLines.map(busLine => <option value={busLine}>{busLine}</option>)
+    const busLineOptionList = busLines.map(busLine => <option key={busLine} value={busLine}>{busLine}</option>)
     return busLineOptionList;
 }
 
--- src/components/StopIdOptionList.tsx
+++ src/components/StopIdOptionList.tsx
@@ -1,7 +1,7 @@
 function StopIdOptionList(){
     const stopIds = [1, 2, 3, 4]
-    const busLineOptionList = stopIds.map(stopId => <option value={stopId}>{stopId}</option>)
-    return busLineOptionList;
+    const stopIdOptionList = stopIds.map(stopId => <option key={stopId} value={stopId}>{stopId}</option>)
+    return stopIdOptionList;
 }
 
 export default StopIdOptionList
