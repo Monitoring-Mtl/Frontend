838c02d65c46fad969f7788770e2d34233c234ea
Jeremy-Gauthier1 <83983417+Jeremy-Gauthier1@users.noreply.github.com>
2023-11-10 16:25:57 -0500

    fix tests

--- tests/components/dummy.test.js
+++ tests/components/dummy.test.js
@@ -1,7 +1,7 @@
 // dummy.test.js
-const { add } = require('../../src/components/dummyTest');
+const { add } = require("../../src/app/components/dummyTest");
 
-test('add function adds two numbers correctly', () => {
-    const result = add(2, 3);
-    expect(result).toBe(5);
+test("add function adds two numbers correctly", () => {
+  const result = add(2, 3);
+  expect(result).toBe(5);
 });
