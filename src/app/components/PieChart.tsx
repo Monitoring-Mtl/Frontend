f61b1d1e597a1f0158707c1fd5d2d276ee022619
Jeremy-Gauthier1 <83983417+Jeremy-Gauthier1@users.noreply.github.com>
2023-11-10 19:42:03 -0500

    fix document undefined warning

--- src/app/components/PieChart.tsx
+++ src/app/components/PieChart.tsx
@@ -8,7 +8,6 @@ import * as d3 from "d3";
 // if (typeof window !== "undefined") {
 
 interface IPieChart {
-  id: string;
   pies: Pie[];
   renderListener?: any;
   colorRange: string[];
@@ -22,12 +21,13 @@ export type Pie = {
 type Margin = { top: number; bottom: number };
 
 export default function PieChart({
-  id,
   pies,
   renderListener,
   colorRange,
 }: IPieChart) {
-  const overlayId = "access-ramp-overlay-" + id;
+  const container = React.useRef<HTMLDivElement>(null);
+  const svg = React.useRef<SVGSVGElement>(null);
+  const overlay = React.useRef<HTMLDivElement>(null);
 
   const computeFontSize = (width: number, height: number) =>
     Math.min(width, height) / 25;
@@ -36,79 +36,86 @@ export default function PieChart({
     Math.min(width, height) / 2 - (margin.top + margin.bottom);
 
   const drawChart = useCallback(
-    (width: number, height: number, margin: Margin) => {
-      d3.select(`#${id}`).select("svg").remove();
+    (
+      svgRef: React.RefObject<SVGSVGElement>,
+      overlayRef: React.RefObject<HTMLDivElement>,
+      width: number,
+      height: number,
+      margin: Margin
+    ) => {
+      if (typeof window !== "undefined") {
+        d3.select(svgRef.current).select("g").remove();
 
-      let colorScale: any = d3.scaleOrdinal().range(colorRange);
+        let colorScale: any = d3.scaleOrdinal().range(colorRange);
 
-      const overlay = d3
-        .select(`#${overlayId}`)
-        .style("position", "fixed")
-        .style("display", "none");
+        const overlay = d3
+          .select(overlayRef.current)
+          .style("position", "fixed")
+          .style("display", "none");
 
-      const svg = d3
-        .select(`#${id}`)
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", `translate(${width * 0.5}, ${height * 0.5})`);
+        const svg = d3
+          .select(svgRef.current)
+          .attr("width", width)
+          .attr("height", height)
+          .append("g")
+          .attr("transform", `translate(${width * 0.5}, ${height * 0.5})`);
 
-      const arcGenerator: any = d3
-        .arc()
-        .innerRadius(0)
-        .outerRadius(computeOuterRadius(width, height, margin));
+        const arcGenerator: any = d3
+          .arc()
+          .innerRadius(0)
+          .outerRadius(computeOuterRadius(width, height, margin));
 
-      const pieGenerator = d3
-        .pie()
-        .padAngle(0)
-        .value((d) => d.valueOf());
+        const pieGenerator = d3
+          .pie()
+          .padAngle(0)
+          .value((d) => d.valueOf());
 
-      const arc = svg
-        .selectAll()
-        .data(pieGenerator(pies.map((d) => d.value)))
-        .enter();
+        const arc = svg
+          .selectAll()
+          .data(pieGenerator(pies.map((d) => d.value)))
+          .enter();
 
-      arc
-        .append("path")
-        .attr("d", arcGenerator)
-        .attr("class", "graph-element")
-        .style("fill", (_, i) => colorScale(i))
-        .style("stroke", "#ffffff")
-        .style("stroke-width", 0);
+        arc
+          .append("path")
+          .attr("d", arcGenerator)
+          .attr("class", "graph-element")
+          .style("fill", (_, i) => colorScale(i))
+          .style("stroke", "#ffffff")
+          .style("stroke-width", 0);
 
-      arc
-        .append("text")
-        .attr("text-anchor", "middle")
-        .attr("alignment-baseline", "middle")
-        .attr("class", "graph-element")
-        .text((d) => d.value)
-        .style("font-size", `${computeFontSize(width, height)}px`)
-        .attr("transform", (d) => {
-          const [x, y] = arcGenerator.centroid(d);
-          return `translate(${x}, ${y})`;
-        });
+        arc
+          .append("text")
+          .attr("text-anchor", "middle")
+          .attr("alignment-baseline", "middle")
+          .attr("class", "graph-element")
+          .text((d) => d.value)
+          .style("font-size", `${computeFontSize(width, height)}px`)
+          .attr("transform", (d) => {
+            const [x, y] = arcGenerator.centroid(d);
+            return `translate(${x}, ${y})`;
+          });
 
-      svg
-        .selectAll(".graph-element")
-        .on("mouseover", function (event, d: any) {
-          const sum: number = pies.reduce((a, b) => a + b.value, 0);
-          overlay
-            .html(`${((d.data / sum) * 100).toFixed(1)}%`)
-            .style("left", event.clientX + 10 + "px")
-            .style("top", event.clientY - 40 + "px")
-            .style("display", "block");
-        })
-        .on("mousemove", function (event, d) {
-          overlay
-            .style("left", event.clientX + 10 + "px")
-            .style("top", event.clientY - 40 + "px");
-        })
-        .on("mouseout", function () {
-          overlay.style("display", "none");
-        });
+        svg
+          .selectAll(".graph-element")
+          .on("mouseover", function (event, d: any) {
+            const sum: number = pies.reduce((a, b) => a + b.value, 0);
+            overlay
+              .html(`${((d.data / sum) * 100).toFixed(1)}%`)
+              .style("left", event.clientX + 10 + "px")
+              .style("top", event.clientY - 40 + "px")
+              .style("display", "block");
+          })
+          .on("mousemove", function (event, d) {
+            overlay
+              .style("left", event.clientX + 10 + "px")
+              .style("top", event.clientY - 40 + "px");
+          })
+          .on("mouseout", function () {
+            overlay.style("display", "none");
+          });
+      }
     },
-    [colorRange, id, pies]
+    [colorRange, pies]
   );
 
   const render = useCallback(() => {
@@ -118,26 +125,30 @@ export default function PieChart({
     };
     let width = 400;
     let height = 400;
-    const element = document.getElementById(id);
-    if (element) {
-      width = element.offsetWidth;
-      height = element.offsetHeight;
+    if (container.current) {
+      width = container.current.offsetWidth;
+      height = container.current.offsetHeight;
     }
 
-    drawChart(width, height, margin);
-  }, [drawChart, id]);
+    drawChart(svg, overlay, width, height, margin);
+  }, [drawChart, svg, overlay, container]);
 
   useEffect(() => {
     render();
   }, [render, renderListener]);
 
   render();
-  window.addEventListener("resize", render);
+  if (typeof window !== "undefined") {
+    window.addEventListener("resize", render);
+  }
 
   return (
-    <div
-      id={overlayId}
-      className="bg-white border rounded-lg px-2 py-2 opacity-90 hidden"
-    ></div>
+    <div ref={container} className="w-full h-full">
+      <svg ref={svg} />
+      <div
+        ref={overlay}
+        className="bg-white border rounded-lg px-2 py-2 opacity-90 hidden"
+      ></div>
+    </div>
   );
 }
