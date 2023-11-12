9007906e42cde314305626128b212ed9db76f2d1
SimonV <sim.voyer@gmail.com>
2023-11-11 20:04:12 -0500

    adjusted title

--- src/app/layouts/BarChart.tsx
+++ src/app/layouts/BarChart.tsx
@@ -4,7 +4,7 @@ import { BarChart } from "../components/graphs/BarChart";
 export const BarChartLayout = ({chartOptions}) => {
     return (
         <>
-            <CardHeader title="Taux d&#39;occupation des autobus"></CardHeader>
+            <CardHeader title="Niveau d&#39;occupation des autobus"></CardHeader>
             <CardContent className="w-full h-full">
                 <BarChart chartOptions={chartOptions}/>
             </CardContent>
