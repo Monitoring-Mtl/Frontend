43fbb7832e22e6500b1075dd2396ac1546ca6c34
SimonV <sim.voyer@gmail.com>
2023-11-22 17:26:42 -0500

    a few improvements to punctuality charts

--- src/app/components/graphs/BusPunctualityChart.tsx
+++ src/app/components/graphs/BusPunctualityChart.tsx
@@ -2,20 +2,21 @@ import { CardContent, CardHeader } from "@mui/material";
 import { ChartjsOptions } from "@/types/ChartjsOptions"
 import * as colorUtils from "@/utils/color-utils";
 import { ScatterPlot } from "./templates/ScatterPlot";
-import { integerDivision } from "@/utils/math-utils";
+import { integerDivision, mean, median } from "@/utils/math-utils";
 
 export const BusPunctualityChart = ({busData}) => {
 
-    const data = busData.map((bus, i) => ({x:i+1, y:bus.punctuality}));
-    const colors = generateColors(busData.map((bus) => bus.punctuality))
+    const scatterFormatData = busData.map((bus, i) => ({x:i+1, y:integerDivision(bus.punctuality, 60)}));
+    const offsets = busData.map((bus) => bus.punctuality);
+    const colors = generateColors(offsets);
 
     const chartOptions : ChartjsOptions = {
-        labels:["Test"],
-        data:data,
+        labels:[],
+        data:scatterFormatData,
         colors:colors,
-        xAxisTitle:"",
-        yAxisTitle:"Décalage avec le temps d'arrivé prévu (secondes)",
-        yAxisBeginAt0:false,
+        xTitle:"",
+        yTitle:"Décalage avec le temps d'arrivé prévu (minutes)",
+        yBeginAt0:false,
         tooltipLabelCallBack:(context) => offsetToString(context.raw.y)
     }
 
@@ -23,13 +24,17 @@ export const BusPunctualityChart = ({busData}) => {
         <>
             <CardHeader title="Ponctualité des autobus"></CardHeader>
             <CardContent id="punctuality-chart" className="w-full h-full">
-                <ScatterPlot chartOptions={chartOptions}/>
+                <div className="flex flex-col w-full h-5/6">
+                    <ScatterPlot chartOptions={chartOptions}/>
+                    <span><strong>Moyenne: </strong>{offsetToString(mean(offsets).toFixed(1))}</span>
+                    <span><strong>Médiane: </strong>{offsetToString(median(offsets).toFixed(1))}</span>
+                </div>
             </CardContent>
         </>
     )
 }
 
-const timeDivisor : number = 1000;
+const timeDivisor : number = 20;
 
 const toColorIndex = (value) => integerDivision(Math.abs(value), timeDivisor);
 
@@ -43,16 +48,14 @@ const generateColors = (data) => {
     return data.map(offset => offset <= 0 ? greens[toColorIndex(offset)] : reds[toColorIndex(offset)]);
 }
 
-const timeUnitToString = (absOffset) => {
-    const numericValue = absOffset < 60 ? absOffset : integerDivision(absOffset, 60);
-    const timeUnit = absOffset < 60 ? " seconde" : " minute";
-
-    return numericValue + (numericValue > 1 ? timeUnit + "s" : timeUnit);
-}
-
 const offsetToString = (offset) => {
-    const timeUnit = timeUnitToString(Math.abs(offset))
+    if (offset === 0){
+        return "Arrivé à temps"
+    }
+
+    const absOffset = Math.abs(offset);
+    const timeUnit = ` ${absOffset > 1 ? "minutes" : "minute"}`
     const timeQualifier = ` ${offset < 0 ? "d'avance" : "de retard"}`
 
-    return timeUnit + timeQualifier;
+    return absOffset + timeUnit + timeQualifier;
 }
