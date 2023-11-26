96703869d624020f1c74b6f3f1dde140ef3eb4d7
Hanh-Long Do <hanh-long.do.1@ens.etsmtl.ca>
2023-11-26 13:35:24 -0500

    npm run build npm run ci:test

---
+++ to_copy_under_.git_hooks/pre-commit copy.ps1
@@ -0,0 +1,17 @@
+npm run build
+npm run ci:test
+
+# Check the exit code to see if tests failed
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Unit tests failed. Commit rejected."
+    exit 1
+}
+
+# Run npm audit to check for vulnerabilities
+npm audit
+
+# Check the exit code to see if vulnerabilities were found
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Vulnerabilities found. Commit rejected."
+    exit 1
+}
