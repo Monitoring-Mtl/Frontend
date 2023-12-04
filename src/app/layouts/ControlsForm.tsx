077b03421fe94f61e9d4eda822fedc557cd5d0c2
Jeremy-Gauthier1 <83983417+Jeremy-Gauthier1@users.noreply.github.com>
2023-12-04 14:50:36 -0500

    spinner

--- src/app/layouts/ControlsForm.tsx
+++ src/app/layouts/ControlsForm.tsx
@@ -5,6 +5,7 @@ import * as yup from "yup";
 import {
   CardContent,
   CardHeader,
+  CircularProgress,
   FormControl,
   InputLabel,
   MenuItem,
@@ -17,9 +18,16 @@ import { Route } from "@/types/Route";
 import { Direction } from "@/types/Direction";
 import { Stop } from "@/types/Stop";
 
-interface IControlsForm{
-    directionCallback: (direction:Direction) => void;
-    stmAnalysisCallback: (routeId:string, stopId:string, startDate:string, startTime:string, endDate:string, endTime:string) => void;
+interface IControlsForm {
+  directionCallback: (direction: Direction) => void;
+  stmAnalysisCallback: (
+    routeId: string,
+    stopId: string,
+    startDate: string,
+    startTime: string,
+    endDate: string,
+    endTime: string
+  ) => void;
 }
 
 type ControlsFormFields = {
@@ -42,7 +50,10 @@ const ControlsFormSchema = yup.object().shape({
   direction: yup.string().required("Required"),
 });
 
-export default function ControlsForm({directionCallback, stmAnalysisCallback} : IControlsForm) {
+export default function ControlsForm({
+  directionCallback,
+  stmAnalysisCallback,
+}: IControlsForm) {
   const [formInitialValues] = useState<ControlsFormFields>({
     busLine: 51,
     direction: "",
@@ -56,12 +67,16 @@ export default function ControlsForm({directionCallback, stmAnalysisCallback} : 
   const [routes, setRoutes] = useState<Route[]>([]);
   const [directions, setDirections] = useState<Direction[] | null>([]);
   const [stops, setStops] = useState<Stop[] | null>([]);
+  const [routeError, setRouteError] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const routes = await ServerlessApiService.getRoutes();
-        setRoutes(routes);
+        const r = await ServerlessApiService.getRoutes();
+        setRoutes(r);
+        if (r.length == 0) {
+          setRouteError("Impossible de récupérer les routes pour le moment.");
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -81,7 +96,7 @@ export default function ControlsForm({directionCallback, stmAnalysisCallback} : 
 
   const updateDirections = async (event) => {
     try {
-      const route = findRouteById(event.target.value)
+      const route = findRouteById(event.target.value);
       setDirections(route?.directions || []);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -112,12 +127,12 @@ export default function ControlsForm({directionCallback, stmAnalysisCallback} : 
       validationSchema={ControlsFormSchema}
       onSubmit={(values: ControlsFormFields) => {
         stmAnalysisCallback(
-            values.busLine.toString(),
-            values.stopId.toString(),
-            values.beginDate,
-            values.beginTime,
-            values.endDate,
-            values.endTime
+          values.busLine.toString(),
+          values.stopId.toString(),
+          values.beginDate,
+          values.beginTime,
+          values.endDate,
+          values.endTime
         );
       }}
     >
@@ -126,109 +141,117 @@ export default function ControlsForm({directionCallback, stmAnalysisCallback} : 
           <div>
             <CardHeader title="Choix de la ligne et de l'arrêt" />
             <CardContent>
-              <FormControl fullWidth>
-                <InputLabel># ligne</InputLabel>
-                <Select
-                  value={values["busLine"]}
-                  label="# ligne"
-                  onChange={(e) => {
-                    setFieldValue("busLine", e.target.value);
-                    updateDirections(e);
-                  }}
-                >
-                  {routes.map((route) => (
-                    <MenuItem key={route.id} value={route.id}>
-                      {route.id} {route.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <FormControl fullWidth>
-                <InputLabel>Direction</InputLabel>
-                <Select
-                  value={values["direction"]}
-                  label="Direction"
-                  onChange={
-                    (e) => {
-                      setFieldValue("direction", e.target.value)
-                      updateStops(e)
-                      const direction = findDirectionByName(e.target.value);
-                      if (direction){
-                        directionCallback(direction)
-                      }
-                    }
-                  }
-                >
-                  {directions?.map((direction) => (
-                    <MenuItem key={direction.name} value={direction.name}>
-                      {direction.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <FormControl fullWidth>
-                <InputLabel># arrêt</InputLabel>
-                <Select
-                  value={values["stopId"]}
-                  label="# arrêt"
-                  onChange={(e) => setFieldValue("stopId", e.target.value)}
-                >
-                  {stops &&
-                    stops.map((stop) => (
-                      <MenuItem key={stop.id} value={stop.id}>
-                        {stop.id} {stop.name}
-                      </MenuItem>
-                    ))}
-                </Select>
-              </FormControl>
+              {routes.length === 0 ? (
+                routeError ? (
+                  routeError
+                ) : (
+                  <CircularProgress />
+                )
+              ) : (
+                <>
+                  <FormControl fullWidth>
+                    <InputLabel># ligne</InputLabel>
+                    <Select
+                      value={values["busLine"]}
+                      label="# ligne"
+                      onChange={(e) => {
+                        setFieldValue("busLine", e.target.value);
+                        updateDirections(e);
+                      }}
+                    >
+                      {routes.map((route) => (
+                        <MenuItem key={route.id} value={route.id}>
+                          {route.id} {route.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormControl fullWidth>
+                    <InputLabel>Direction</InputLabel>
+                    <Select
+                      value={values["direction"]}
+                      label="Direction"
+                      onChange={(e) => {
+                        setFieldValue("direction", e.target.value);
+                        updateStops(e);
+                        const direction = findDirectionByName(e.target.value);
+                        if (direction) {
+                          directionCallback(direction);
+                        }
+                      }}
+                    >
+                      {directions?.map((direction) => (
+                        <MenuItem key={direction.name} value={direction.name}>
+                          {direction.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormControl fullWidth>
+                    <InputLabel># arrêt</InputLabel>
+                    <Select
+                      value={values["stopId"]}
+                      label="# arrêt"
+                      onChange={(e) => setFieldValue("stopId", e.target.value)}
+                    >
+                      {stops &&
+                        stops.map((stop) => (
+                          <MenuItem key={stop.id} value={stop.id}>
+                            {stop.id} {stop.name}
+                          </MenuItem>
+                        ))}
+                    </Select>
+                  </FormControl>
 
-              <Field
-                component={TextField}
-                fullWidth
-                name="beginDate"
-                label="Date de début"
-                type="date"
-                onChange={(e) => setFieldValue("beginDate", e.target.value)}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
+                  <Field
+                    component={TextField}
+                    fullWidth
+                    name="beginDate"
+                    label="Date de début"
+                    type="date"
+                    onChange={(e) => setFieldValue("beginDate", e.target.value)}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
 
-              <Field
-                component={TextField}
-                fullWidth
-                name="beginTime"
-                label="Heure de début"
-                type="time"
-                onChange={(e) => setFieldValue("beginTime", e.target.value)}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
+                  <Field
+                    component={TextField}
+                    fullWidth
+                    name="beginTime"
+                    label="Heure de début"
+                    type="time"
+                    onChange={(e) => setFieldValue("beginTime", e.target.value)}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
 
-              <Field
-                component={TextField}
-                fullWidth
-                name="endDate"
-                label="Date de fin"
-                type="date"
-                onChange={(e) => setFieldValue("endDate", e.target.value)}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
+                  <Field
+                    component={TextField}
+                    fullWidth
+                    name="endDate"
+                    label="Date de fin"
+                    type="date"
+                    onChange={(e) => setFieldValue("endDate", e.target.value)}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
 
-              <Field
-                component={TextField}
-                fullWidth
-                name="endTime"
-                label="Heure de fin"
-                type="time"
-                onChange={(e) => setFieldValue("endTime", e.target.value)}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
+                  <Field
+                    component={TextField}
+                    fullWidth
+                    name="endTime"
+                    label="Heure de fin"
+                    type="time"
+                    onChange={(e) => setFieldValue("endTime", e.target.value)}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </>
+              )}
             </CardContent>
           </div>
           <FullButton onClick={() => submitForm()}>Analyser</FullButton>
